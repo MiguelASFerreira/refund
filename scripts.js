41e32976ceafce65f69ae30eff7ca1d3149f28c0
MiguelASFerreira <migant1306@gmail.com>
2024-10-02 21:19:48 -0300

    Removendo um item da lista

--- scripts.js
+++ scripts.js
@@ -158,6 +158,13 @@ function updateTotals() {
 expenseList.addEventListener("click", (event) => {
     // Verifica se o elemento clicado é o ícone de remover
     if (event.target.classList.contains("remove-icon")) {
-        console.log(event)
+        // Obtém a li pai do elemento clicado
+        const item = event.target.closest(".expense")
+
+        // Remove o item da lista
+        item.remove()
     }
+
+    // Atualiza os totais
+    updateTotals()
 })
